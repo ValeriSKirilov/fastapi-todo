05e73ebb9cec565afee49affb355035fffe028d1
Valeri Kirilov <valeri.stoyanov2005@gmail.com>
2026-05-25 22:44:12 +0300

    Modular APIRouter architecture implementation

--- app/routers/items.py
+++ app/routers/items.py
@@ -1,23 +1,16 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-
-app = FastAPI()
-
-
-class Item(BaseModel):
-    text: str
-    is_done: bool = False
-
+from fastapi import APIRouter, HTTPException
+from app.models import Item
 
 items = []
 
+router = APIRouter(
+    prefix="/items",
+    tags=["items"],
+    responses={404: {"description": "Not found"}},
+)
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
 
-
-@app.get("/items", response_model=list[Item])
+@router.get("", response_model=list[Item])
 def list_items(limit: int = None):
     if limit is None:
         return items
@@ -25,13 +18,13 @@ def list_items(limit: int = None):
         return items[0:limit]
 
 
-@app.post("/items", response_model=list[Item])
+@router.post("", response_model=list[Item])
 def create_item(item: Item):
     items.append(item)
     return items
 
 
-@app.get("/items/{item_id}", response_model=Item)
+@router.get("/{item_id}", response_model=Item)
 def get_item(item_id: int) -> Item:
     if item_id < len(items):
         return items[item_id]
@@ -39,7 +32,7 @@ def get_item(item_id: int) -> Item:
         raise HTTPException(status_code=404, detail="Item not found")
 
 
-@app.delete("/items/{item_id}")
+@router.delete("/{item_id}")
 def remove_item(item_id: int):
     if item_id < len(items):
         del items[item_id]
